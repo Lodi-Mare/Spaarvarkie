09ec1c1611f0f4268ff457b7ea7cd2a12344ddae
Lodi-Mare <lodimare@hotmail.com>
2023-12-05 11:01:41 +0200

    add database connection

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express')
 const app = express()
-const bcrypt = require('bcrypt')
-const passport = require('passport')
-const flash = require('express-flash')
-const session = require('express-session')
-const methodOverride = require('method-override')
-const expressLayouts = require('express-ejs-layouts')
+// const bcrypt = require('bcrypt')
+// const passport = require('passport')
+// const flash = require('express-flash')
+// const session = require('express-session')
+// const methodOverride = require('method-override')
+// const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
 
-const initializePassport = require('./passport-config')
-initializePassport(
-    passport,
-    email => users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
-)
+// const initializePassport = require('./passport-config')
+// initializePassport(
+//     passport,
+//     email => users.find(user => user.email === email),
+//     id => users.find(user => user.id === id)
+// )
 
 // don't use this in production -- we store data in database
 const users = [];
@@ -28,18 +28,18 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 
-app.use(express.urlencoded({ extended: false }))
-app.use(flash())
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(methodOverride('_method'))
-app.use(expressLayouts)
-app.use(express.static('public'))
+// app.use(express.urlencoded({ extended: false }))
+// app.use(flash())
+// app.use(session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false
+// }))
+// app.use(passport.initialize())
+// app.use(passport.session())
+// app.use(methodOverride('_method'))
+// app.use(expressLayouts)
+// app.use(express.static('public'))
 
 app.use('/', indexRouter)
 //db
